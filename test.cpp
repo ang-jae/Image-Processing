5b22573054c84dc5e9cfe9fd17be0c7e0773cbe4
ang-jae <49384268+ang-jae@users.noreply.github.com>
2019-04-08 14:54:53 +0900

    Rendering2D
    Rendering2D

---
+++ test.cpp
@@ -0,0 +1,756 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+
+#include <opencv2/opencv.hpp>   
+#include <opencv2/core/core.hpp>   
+#include <opencv2/highgui/highgui.hpp>  
+
+using namespace cv;
+
+#define PI 3.14159265359
+
+typedef struct {
+	int r, g, b;
+}int_rgb;
+
+
+int** IntAlloc2(int height, int width)
+{
+	int** tmp;
+	tmp = (int**)calloc(height, sizeof(int*));
+	for (int i = 0; i < height; i++)
+		tmp[i] = (int*)calloc(width, sizeof(int));
+	return(tmp);
+}
+
+void IntFree2(int** image, int height, int width)
+{
+	for (int i = 0; i < height; i++)
+		free(image[i]);
+
+	free(image);
+}
+
+float** FloatAlloc2(int height, int width)
+{
+	float** tmp;
+	tmp = (float**)calloc(height, sizeof(float*));
+	for (int i = 0; i < height; i++)
+		tmp[i] = (float*)calloc(width, sizeof(float));
+	return(tmp);
+}
+
+void FloatFree2(float** image, int height, int width)
+{
+	for (int i = 0; i < height; i++)
+		free(image[i]);
+
+	free(image);
+}
+
+int_rgb** IntColorAlloc2(int height, int width)
+{
+	int_rgb** tmp;
+	tmp = (int_rgb**)calloc(height, sizeof(int_rgb*));
+	for (int i = 0; i < height; i++)
+		tmp[i] = (int_rgb*)calloc(width, sizeof(int_rgb));
+	return(tmp);
+}
+
+void IntColorFree2(int_rgb** image, int height, int width)
+{
+	for (int i = 0; i < height; i++)
+		free(image[i]);
+
+	free(image);
+}
+
+int** ReadImage(char* name, int* height, int* width)
+{
+	Mat img = imread(name, IMREAD_GRAYSCALE);
+	int** image = (int**)IntAlloc2(img.rows, img.cols);
+
+	*width = img.cols;
+	*height = img.rows;
+
+	for (int i = 0; i < img.rows; i++)
+		for (int j = 0; j < img.cols; j++)
+			image[i][j] = img.at<unsigned char>(i, j);
+
+	return(image);
+}
+
+void WriteImage(char* name, int** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC1);
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
+
+	imwrite(name, img);
+}
+
+
+void ImageShow(char* winname, int** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC1);
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
+	imshow(winname, img);
+	waitKey(0);
+}
+
+
+
+int_rgb** ReadColorImage(char* name, int* height, int* width)
+{
+	Mat img = imread(name, IMREAD_COLOR);
+	int_rgb** image = (int_rgb**)IntColorAlloc2(img.rows, img.cols);
+
+	*width = img.cols;
+	*height = img.rows;
+
+	for (int i = 0; i < img.rows; i++)
+		for (int j = 0; j < img.cols; j++) {
+			image[i][j].b = img.at<Vec3b>(i, j)[0];
+			image[i][j].g = img.at<Vec3b>(i, j)[1];
+			image[i][j].r = img.at<Vec3b>(i, j)[2];
+		}
+
+	return(image);
+}
+
+void WriteColorImage(char* name, int_rgb** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC3);
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++) {
+			img.at<Vec3b>(i, j)[0] = (unsigned char)image[i][j].b;
+			img.at<Vec3b>(i, j)[1] = (unsigned char)image[i][j].g;
+			img.at<Vec3b>(i, j)[2] = (unsigned char)image[i][j].r;
+		}
+
+	imwrite(name, img);
+}
+
+void ColorImageShow(char* winname, int_rgb** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC3);
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++) {
+			img.at<Vec3b>(i, j)[0] = (unsigned char)image[i][j].b;
+			img.at<Vec3b>(i, j)[1] = (unsigned char)image[i][j].g;
+			img.at<Vec3b>(i, j)[2] = (unsigned char)image[i][j].r;
+		}
+	imshow(winname, img);
+
+}
+
+template <typename _TP>
+void ConnectedComponentLabeling(_TP** seg, int height, int width, int** label, int* no_label)
+{
+
+	//Mat bw = threshval < 128 ? (img < threshval) : (img > threshval);
+	Mat bw(height, width, CV_8U);
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			bw.at<unsigned char>(i, j) = (unsigned char)seg[i][j];
+	}
+	Mat labelImage(bw.size(), CV_32S);
+	*no_label = connectedComponents(bw, labelImage, 8); // 0까지 포함된 갯수임
+
+	(*no_label)--;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			label[i][j] = labelImage.at<int>(i, j);
+	}
+}
+
+void main_0305()
+{
+	int height, width;
+	int** image = ReadImage((char*)"pG.jpg", &height, &width);
+
+	//printf("height = %d, width = %d", height, width);
+	for (int y = 0; y < 300; y++)
+		for (int x = 0; x < 300; x++)
+		{
+			image[y][x] = 255;
+		}
+
+	ImageShow((char*)"test", image, height, width);
+
+}
+void main_0312()//0312
+{
+	int height, width;
+	int** image = ReadImage((char*)"pG.jpg", &height, &width);
+
+	//printf("height = %d, width = %d", height, width);
+	int thick = 20;
+	for (int y = 0; y < 300; y++)
+		for (int x = 0; x < 300; x++)
+		{
+			for (int z = (-1)*thick; z <= thick; z++)
+			{
+				y = (int)(2 * x + 100 + 0.5);
+				//image[y+z][x+z] = 255; 예외처리를 해 주어야 한다(width-1, height-1를 넘는 경우)
+				if (y < 0 || y >= height - 1) continue;
+				else image[y + z][x + z] = 255;
+			}
+		}
+
+	ImageShow((char*)"test", image, height, width);
+
+}
+
+void DrawLine(int**image, int height, int width, double a, double b,
+	double Thickness)
+{
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			double d = fabs(a*x - y + b) / sqrt(a*a + 1.0);
+
+			if (d < Thickness) {
+				image[y][x] = 0;
+			}
+		}
+	}
+
+
+}
+void DrawCircle(int**image, int height, int width, int a, int b, double r) //(a,b)
+{
+
+	for (int x = -r; x < r; x++)
+	{
+		int y = (int)(sqrt(r*r - x * x));
+		image[b + y][a + x] = 255;
+		image[b - y][a + x] = 255;
+	}
+
+	for (int y = -r; y < r; y++)
+	{
+		int x = (int)(sqrt(r*r - y * y));
+		image[b + y][a + x] = 255;
+		image[b + y][a - x] = 255;
+	}
+}
+
+void DrawCircleFull(int**image, int height, int width, int a, int b, double r) //(a,b)
+{
+	for (int x = -r; x < r; x++)
+	{
+		int y = (int)(sqrt(r*r - x * x));
+		for (int i = -y; i < y; i++)
+		{
+			image[b + i][a + x] = 255;
+		}
+	}
+}
+
+
+void main_0312_2()//점과 직선 사이의 공식 이용
+{
+	int height, width;
+	int** image = ReadImage((char*)"pG.jpg", &height, &width);
+
+	//y= ax+b -> ax-y+b = 0 -> d = |ax0 - y0 + b| / sqrt(a*a + 1)
+	//함수화. 입력 : a, b, Thickness, image, width, height
+	double a = 1.0;
+	double b = 50.0;
+	double Thickness = 3.0;
+	DrawLine(image, height, width, a, b, Thickness);
+	DrawCircle(image, height, width, width / 2, height / 2, 100);
+	DrawCircleFull(image, height, width, width / 2, height / 2, 50);
+	//	for(int i=0.1; i<10; i+=0.1)DrawLine(image, height, width, i, b, Thickness);
+
+
+	ImageShow((char*)"test", image, height, width);
+}
+
+void main_0313()
+{
+	//03/13 affine transform
+
+	int height, width;
+	int** image = ReadImage((char*)"pG.jpg", &height, &width);
+	int** image_out = IntAlloc2(height, width);
+	float a, b, c, d, t1, t2;
+	a = -1; b = 0; c = 0; d = -1;
+	//t1 = 0; t2 = 0;
+	t1 = width, t2 = height;
+
+
+	for (int i = 1; i < height; i++)
+		for (int j = 1; j < width; j++)
+		{
+			if (a*j + b * i + t1 >= width || c * j + d * i + t2 >= height || a * j + b * i + t1 < 0 || c * j + d * i + t2 < 0) continue;
+			else image_out[(int)(c*j + d * i + t2)][(int)(a*j + b * i + t1)] = image[i][j];
+		}
+
+
+	ImageShow((char*)"test", image, height, width);
+	ImageShow((char*)"test", image_out, height, width);
+}
+
+
+#define imax(x, y) ((x)>(y) ? x : y)
+#define imin(x, y) ((x)<(y) ? x : y)
+/*
+int BilinearInterpolation(int** image, int width, int height, double x, double y)
+{
+int x_int = (int)x;
+int y_int = (int)y;
+
+int A = image[imin(imax(y_int, 0), height - 1)][imin(imax(x_int, 0), width - 1)];
+int B = image[imin(imax(y_int, 0), height - 1)][imin(imax(x_int + 1, 0), width - 1)];
+int C = image[imin(imax(y_int + 1, 0), height - 1)][imin(imax(x_int, 0), width - 1)];
+int D = image[imin(imax(y_int + 1, 0), height - 1)][imin(imax(x_int + 1, 0), width - 1)];
+
+double dx = x - x_int;
+double dy = y - y_int;
+
+double value
+= (1.0 - dx)*(1.0 - dy)*A + dx*(1.0 - dy)*B
++ (1.0 - dx)*dy*C + dx*dy*D;
+
+return((int)(value + 0.5));
+}
+*/
+
+int BilinearInterpolation(int**image, int width, int height, double xx, double yy)
+{
+	int A_x = (int)xx; int A_y = (int)yy;
+	int B_x = A_x + 1; int B_y = A_y;
+	int C_x = A_x; int C_y = A_y + 1;
+	int D_x = A_x + 1; int D_y = A_y + 1;
+
+
+#if 0
+	A_x = imin(imax(A_x, 0), width - 1);	B_x = imin(imax(B_x, 0), width - 1);	C_x = imin(imax(C_x, 0), width - 1);	D_x = imin(imax(D_x, 0), width - 1);
+	A_y = imin(imax(A_y, 0), height - 1);	B_y = imin(imax(B_y, 0), height - 1);	C_y = imin(imax(C_y, 0), height - 1);	D_y = imin(imax(D_y, 0), height - 1);
+
+#else
+	if (A_x<0 || A_x > width - 1 || B_x<0 || B_x > width - 1 || C_x<0 || C_x > width - 1 || D_x<0 || D_x > width - 1
+		|| A_y<0 || A_y > height - 1 || B_y<0 || B_y > height - 1 || C_y<0 || C_y > height - 1 || D_y<0 || D_y > height - 1)
+		return (0);
+
+#endif
+
+	double dx = xx - A_x;
+	double dy = yy - A_y;
+
+	int X = image[A_y][A_x] * (1 - dx) * (1 - dy) + image[B_y][B_x] * dx * (1 - dy) + image[C_y][C_x] * (1 - dx) * dy + image[D_y][D_x] * dx * dy;
+
+	return X;
+}
+
+
+/* 03/19
+쌍선형 보간 Bilinear Interpolation
+x = 1/(ad-bc) *{d(x'-t1) - b*(y'-t2)}
+y = 1/(ad-bc) *{-c(y'-t1) + a*(y'-t2)}
+*/
+void main_0319()
+{
+	int height, width;
+	int** image = ReadImage((char*)"pG.jpg", &height, &width);
+	int** image_out = IntAlloc2(height, width);
+	float a, b, c, d, t1, t2;
+	a = 2; b = 0; c = 0; d = 2;
+	t1 = 0; t2 = 0;
+	//t1 = width, t2 = height;
+
+
+	for (int y2 = 1; y2 < height; y2++)
+		for (int x2 = 1; x2 < width; x2++)
+		{
+			int x = 1 / (a*d - b * c)*(d*(x2 - t1) - b * (y2 - t2));
+			int y = 1 / (a*d - b * c)*((-1)*c*(x2 - t1) + a * (y2 - t2));
+
+			if (x >= width || y >= height || x < 0 || y < 0) continue;
+			else image_out[y2][x2] = BilinearInterpolation(image, width, height, x, y);
+		}
+
+
+	ImageShow((char*)"test", image, height, width);
+	ImageShow((char*)"test", image_out, height, width);
+}
+
+//가운데 중심으로 돌리기 -> 숙제
+
+void main_0320()
+{
+
+	int width, height, width_out, height_out;
+	float x, y;
+	int** image = ReadImage((char*)"pG.jpg", &width, &height);
+	int** image_out = IntAlloc2(width, height);
+
+	double theta = 45 * (3.14 / 180);
+
+
+	for (int v = 0; v < height; v++)
+		for (int u = 0; u < width; u++)
+		{
+			x = cos(theta)*(u - width / 2) + sin(theta)*(v - height / 2) + width / 2;
+			y = sin(theta)*(u - width / 2) + cos(theta)*(v - height / 2) + height / 2;
+
+			if (x >= width || y >= height || x < 0 || y < 0) continue;
+			else image_out[v][u] = BilinearInterpolation(image, width, height, (int)x, (int)y);
+		}
+
+
+	ImageShow((char*)"original", image, width, height);
+	ImageShow((char*)"output", image_out, width, height);
+
+
+
+	IntFree2(image, width, height);
+	IntFree2(image_out, width, height);
+}
+/*
+x' = (a b)  x  + t1
+y'	 (c d)  y	   t2
+에서
+(x'-w/2-t1) = (a b)  (x-w/2) ==>  (a b)-1(x'-w/2-t1) + (w/2) = (x) ==> 1(d -b)(x' - w/2 - t1) + w/2
+(y'-h/2-t1) = (c d)  (y-h/2)      (c d)  (y'-h/2-t2)   (h/2)   (y)     D(-c a)(y' - h/2 - t1) + h/2
+*/
+void main_0320_2()
+{
+	int height, width;
+	int** image = ReadImage((char*)"pG.jpg", &height, &width);
+	int** image_out = IntAlloc2(height, width);
+	float a, b, c, d, t1, t2;
+	double theta = 45 * (3.14 / 180);
+	a = cos(theta);
+	b = (-1)*sin(theta);
+	c = sin(theta);			//크기를 많이 줄이면 -> 반짝거리는 현상(에얼리어싱)이 발생한다.
+	d = cos(theta);			//해결하려면 Lowpass Filtering이 필요하다.
+	double det = a * d - b * c;
+	t1 = 0; t2 = 0;
+	//t1 = width, t2 = height;
+
+
+	for (int y2 = 1; y2 < height; y2++)
+		for (int x2 = 1; x2 < width; x2++)
+		{
+			int x = 1 / det * (d*(x2 - width / 2 - t1) - b * (y2 - height / 2 - t2)) + width / 2;
+			int y = 1 / det * ((-1)*c*(x2 - width / 2 - t1) + a * (y2 - height / 2 - t2)) + height / 2;
+
+			if (x >= width || y >= height || x < 0 || y < 0) continue;
+			else image_out[y2][x2] = BilinearInterpolation(image, width, height, x, y);
+		}
+
+
+	ImageShow((char*)"test", image, height, width);
+	ImageShow((char*)"test", image_out, height, width);
+}
+
+struct POS3D {
+	float x, y, z;
+};
+
+POS3D SetPOS3D(float x, float y, float z)
+{
+	POS3D tmp;
+	tmp.x = x; tmp.y = y; tmp.z = z;
+	return (tmp);
+}
+
+POS3D PlusPOS3D(POS3D p, POS3D q)
+{
+	POS3D s;
+	s.x = p.x + q.x;	s.y = p.y + q.y;	s.z = p.z + q.z;
+	return s;
+}
+POS3D PlusPOS3DTriple(POS3D p, POS3D q, POS3D r)
+{
+	POS3D s;
+	s.x = p.x + q.x + r.x;	s.y = p.y + q.y + r.y;	s.z = p.z + q.z + r.z;
+	return s;
+}
+POS3D MinusPOS3D(POS3D p, POS3D q)
+{
+	POS3D s;
+	s.x = p.x - q.x;	s.y = p.y - q.y;	s.z = p.z - q.z;
+	return s;
+}
+
+struct POS2D {
+	float x, y;
+};
+
+POS2D SetPOS2D(float x, float y)
+{
+	POS2D tmp;
+	tmp.x = x; tmp.y = y;;
+	return (tmp);
+}
+
+POS2D PlusPOS2D(POS3D p, POS3D q)
+{
+	POS2D s;
+	s.x = p.x + q.x;	s.y = p.y + q.y;
+	return s;
+}
+POS2D MinusPOS2D(POS3D p, POS3D q)
+{
+	POS2D s;
+	s.x = p.x - q.x;	s.y = p.y - q.y;
+	return s;
+}
+
+POS3D Ax_3(float**A, POS3D p)//행렬 A와 구조체 x를 곱해주는 함수
+{
+	POS3D s;
+	s.x = A[0][0] * p.x + A[0][1] * p.y + A[0][2] * p.z;
+	s.y = A[1][0] * p.x + A[1][1] * p.y + A[1][2] * p.z;
+	s.z = A[2][0] * p.x + A[2][1] * p.y + A[2][2] * p.z;
+	return s;
+}
+
+#define SQ(x) ((x)*(x))
+#define NP 100 // Num of Points
+
+void ImageShowFloat(char* winname, float** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC1);
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
+	imshow(winname, img);
+	waitKey(0);
+}
+void SetMatrixA(int width, int height, float** A)//2차원배열 A를 설정하는 함수
+{
+	int f = 300;
+	POS2D p0;
+	p0.x = width / 2; p0.y = height / 2;
+
+	A[0][0] = f; A[0][1] = 0; A[0][2] = p0.x;
+	A[1][0] = 0; A[1][1] = f; A[1][2] = p0.y;
+	A[2][0] = 0; A[2][1] = 0; A[2][2] = 1;
+}
+void DrawDot(int**image, POS3D input, int width, int height)
+{
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			if ((int)input.y < 0 || (int)input.x < 0 || (int)input.y >= height || (int)input.x >= width) continue;
+			else image[(int)input.y][(int)input.x] = 255;
+		}
+	}
+}
+void DrawLinePQ(POS3D p, POS3D q, float**A, int**image, int height, int width)//두 점 p와 q를 잇는 선분을 그리는 함수
+{
+	float length = sqrt(SQ(p.x - q.x) + SQ(p.y - q.y) + SQ(p.z - q.z));
+	float delta = length / NP;
+	POS3D diff = MinusPOS3D(q, p); // q-p 차 벡터
+	/*
+	line[0].x = p.x + 0 * delta * diff.x;
+	line[0].y = p.y + 0 * delta * diff.y;
+	line[0].z = p.z + 0 * delta * diff.z;
+
+	line[1].x = p.x + 1 * delta * diff.x;
+	line[1].y = p.y + 1 * delta * diff.y;
+	line[1].z = p.z + 1 * delta * diff.z;
+	... => for loop
+	*/
+	/*
+	for (int i = 0; i < NP; i++)
+	{
+	line[i].x = p.x + i * delta * diff.x;
+	line[i].y = p.y + i * delta * diff.y;
+	line[i].z = p.z + i * delta * diff.z;
+	output[i] = Ax_3(A, line[i]);
+	//이를 이용하여 선분 그리기 -> 과제
+	DrawDot(image, line[i], width, height);
+	}
+	ImageShow((char*)"test", image, height, width);
+	*/
+	//190402
+	//더 간단하게 하면
+	for (int i = 0; i < NP; i++) {
+		POS3D output, line;
+		float t = (float)i / NP;
+		line.x = p.x + t * diff.x;
+		line.y = p.y + t * diff.y;
+		line.z = p.z + t * diff.z;
+
+		output = Ax_3(A, line);
+
+		int x = (int)(output.x / output.z + 0.5);
+		int y = (int)(output.y / output.z + 0.5);
+
+		if (x >= width || x < 0 || y >= height || y < 0)continue;
+		else
+		{
+			image[y][x] = 255;
+			//printf("\n(x,y) = (%d, %d)", x, y);
+		}
+	}
+	//ImageShow((char*)"output", image, height, width);
+}
+void DrawRectangle(POS3D p1, POS3D p2, POS3D p3, POS3D p4, float**A, int**image, int height, int width)//p1-p2, p2-p4, p3-p4, p3-p1을 연결하여 직사각형을 그려주는 함수
+{
+	DrawLinePQ(p1, p2, A, image, height, width);
+	DrawLinePQ(p2, p4, A, image, height, width);
+	DrawLinePQ(p3, p4, A, image, height, width);
+	DrawLinePQ(p3, p1, A, image, height, width);
+}
+void main_0402()
+{
+	int height = 500; int width = 500;
+	int** image = (int**)IntAlloc2(height, width);
+	float** A = (float**)FloatAlloc2(3, 3);
+	/*
+	float qx, qy, qz;
+	float px, py, pz;
+	float sx, sy, sz;
+
+	qx = 1.0; qy = 2.0; qz = 3.0;
+	px = 2.0; py = 2.0; pz = 3.0;
+	sx = qx + px;
+	sy = qy + py;
+	sz = qz + pz;
+	printf("\n(%f, %f, %f) + (%f, %f, %f) = (%f, %f, %f)", qx, qy, qz, px, py, pz, sx, sy, sz);
+	*/
+	//위와 같이 하면 불편. 구조체 이용
+	/*
+	struct POS3D {
+	float x, y, z;
+	};
+
+	POS3D p, q, s;
+	p.x = 1.0; p.y = 2.0; p.z = 3.0;
+	q.x = 2.0; q.y = 2.0; q.z = 3.0;
+	s.x = q.x + p.x;
+	s.y = q.y + p.y;
+	s.z = q.z + p.z;
+	printf("\n(%f, %f, %f) + (%f, %f, %f) = (%f, %f, %f)", q.x, q.y, q.z, p.x, p.y, p.z, s.x, s.y, s.z);
+	*/
+	//이 역시 크게 다르지 않다. 함수화
+	SetMatrixA(width, height, A);
+	POS3D p1 = SetPOS3D(100, 200, 600);
+	POS3D q1 = SetPOS3D(100, -200, 500);
+	DrawLinePQ(p1, q1, A, image, height, width);
+
+	POS3D q2 = SetPOS3D(100, -100, 400);
+	POS3D p2 = SetPOS3D(100, 300, 500);
+	DrawLinePQ(p2, q2, A, image, height, width);
+
+	POS3D p3 = SetPOS3D(100, 300, 600);
+	POS3D q3 = SetPOS3D(100, -200, 500);
+	DrawLinePQ(p3, q3, A, image, height, width);
+
+	POS3D p4 = SetPOS3D(100, 100, 500);
+	POS3D q4 = SetPOS3D(100, 200, 300);
+	DrawLinePQ(p4, q4, A, image, height, width);
+
+	ImageShow((char*)"output", image, height, width);
+}
+
+void main_0402_2()//이 방식은 기준점(p1)에서부터 x,y축으로 i만큼 벌어진 직사각형을 여러 번 그려서 채우는 방식으로, 원하는 바와는 조금 다르다.
+{
+	int height = 1000; int width = 1000;
+	int** image = (int**)IntAlloc2(height, width);
+	float** A = (float**)FloatAlloc2(3, 3);
+	SetMatrixA(width, height, A);
+	POS3D a, b, p1, p2, p3, p4;
+
+	for (int i = 0; i < 100; i++) {
+		a = SetPOS3D(i, 0, 0);
+		b = SetPOS3D(0, i, 0);
+
+		p1 = SetPOS3D(100, 200, 600);
+		p2 = PlusPOS3D(p1, a);
+		p3 = PlusPOS3D(p1, b);
+		p4 = PlusPOS3DTriple(p1, a, b);
+
+		DrawRectangle(p1, p2, p3, p4, A, image, height, width);
+	}
+	ImageShow((char*)"output", image, height, width);
+}
+void main_0402_3()//p1에서 a, b만큼 떨어진 점 x를 여러 번 그리는 방식 -> 부적절
+{
+	int height = 1000; int width = 1000;
+	int** image = (int**)IntAlloc2(height, width);
+	float** A = (float**)FloatAlloc2(3, 3);
+	SetMatrixA(width, height, A);
+	POS3D a, b, p;
+	p = SetPOS3D(100, 200, 600);
+
+	for (float i = 0; i < 200; i++)
+	{
+		for (float j = 0; j < 150; j++)
+		{
+			POS3D x, output;
+			a = SetPOS3D(i, 0, 0);
+			b = SetPOS3D(0, j, 0);
+			x = PlusPOS3DTriple(p, a, b);
+			output = Ax_3(A, x);
+			DrawDot(image, output, width, height);
+		}
+	}
+	ImageShow((char*)"output", image, height, width);
+}
+POS3D MultiPOS3D(float a, POS3D p)
+{
+	POS3D s;
+	s.x = p.x * a; s.y = p.y * a; s.z = p.y * a;
+	return s;
+}
+//p1,p2,p3좌표(직사각형이므로)를 주어주고, 그 내부의 점들을 모두 계산하는 방식, 함수화 할 것, 내부에 사진 넣을 것 ==> 렌더링(Rendering) : 3차원상의 영상을 2차원으로
+
+void Rendering2D(POS3D p1, POS3D p2, POS3D p3, int**image, int height, int width)
+{
+	float** A = (float**)FloatAlloc2(3, 3);
+	SetMatrixA(width, height, A);
+	POS3D v21, v31;
+
+	v21 = MinusPOS3D(p2, p1);
+	v31 = MinusPOS3D(p3, p1);
+	for (float t1 = 0.0; t1 < 1.0; t1 += (float)1 / NP)
+	{
+		for (float t2 = 0.0; t2 < 1.0; t2 += (float)1 / NP)
+		{
+			POS3D q, output;
+			q = PlusPOS3DTriple(p1, MultiPOS3D(t1, v21), MultiPOS3D(t2, v31));
+			output = Ax_3(A, q);
+
+			int x = (int)(output.x / output.z + 0.5);
+			int y = (int)(output.y / output.z + 0.5);
+
+			if (x >= width || x < 0 || y >= height || y < 0)continue;
+			else
+			{
+				image[y][x] = 255;
+			}
+		}
+	}
+	ImageShow((char*)"test", image, height, width);
+}
+
+void main()
+{
+	int height = 1000; int width = 1000;
+	int** image = (int**)IntAlloc2(height, width);
+
+	POS3D p1, p2, p3;
+	p1 = SetPOS3D(100, 200, 600);
+	p2 = SetPOS3D(300, 200, 600);
+	p3 = SetPOS3D(100, 400, 600);
+	Rendering2D(p1, p2, p3, image, height, width);
+
+}
+//위와 비슷한 방식으로 큐브 만들고, 면에다가 사진을 넣을 것(추가점수), 과제 제출할 때 소스랑 결과 캡쳐
